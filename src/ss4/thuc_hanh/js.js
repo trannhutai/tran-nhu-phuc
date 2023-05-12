98b887d464fcb1d0eb641fba054d7f49b5b15f74
trannhutai <trannhutai02112004>
2023-05-12 19:59:26 +0700

    bai tap

--- src/ss4/thuc_hanh/js.js
+++
@@ -1,9 +0,0 @@
-var demo = document.getElementById("demo");
-demo.addEventListener("mouseover",mouseover);
-demo.addEventListener("mouseout",mouseout);
-function mouseover() {
-    document.getElementById("demo").style.color="red";
-}
-function mouseout() {
-    document.getElementById("demo").style.color="blue";
-}
